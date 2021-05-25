fb2707e3fe241e26d3e8ea04160396d135f9490f
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-05-24 23:05:16 -0700

    Put the form's input values into a new object.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var $newEntry = document.querySelector('.entry-form');
 var $saveButton = document.querySelector('.save-button');
 
 $photoUrl.addEventListener('input', handleUrl);
-$saveButton.addEventListener('click', handleSave);
+$newEntry.addEventListener('submit', handleSave);
 
 function handleUrl(event) {
   var $imgPreview = document.querySelector('.img-preview');
@@ -16,11 +16,20 @@ function handleUrl(event) {
   //   $imgPreview.setAttribute('src', event.target.value);
   // } else {
   //   $imgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
-  }
+  // }
 }
 
 function handleSave(event) {
   event.preventDefault();
 
+  var $title = document.querySelector('#entry-title');
+  var $url = document.querySelector('#entry-url');
+  var $notes = document.querySelector('#entry-notes');
+  var entry = {
+    title: $title.value,
+    url: $url.value,
+    notes: $notes.value
+  };
 
+  $newEntry.reset();
 }
