b383d949e941ae9335f2b441cb9e4f0a971c660e
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-05-25 15:51:09 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ var $newEntry = document.querySelector('.entry-form');
 var $imgPreview = document.querySelector('.img-preview');
 var $photoUrl = document.querySelector('.photo-url');
 
-var $title = document.querySelector('#new-title');
-var $url = document.querySelector('#new-url');
-var $notes = document.querySelector('#new-notes');
+var $newTitle = document.querySelector('#new-title');
+var $newUrl = document.querySelector('#new-url');
+var $newNotes = document.querySelector('#new-notes');
 
 $photoUrl.addEventListener('input', handleUrl);
 $imgPreview.addEventListener('error', handleImgError);
@@ -24,9 +24,9 @@ function handleSave(event) {
   event.preventDefault();
 
   var entry = {
-    title: $title.value,
-    url: $url.value,
-    notes: $notes.value,
+    title: $newTitle.value,
+    url: $newUrl.value,
+    notes: $newNotes.value,
     id: data.nextEntryId
   };
 
@@ -36,4 +36,52 @@ function handleSave(event) {
   $imgPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
 
   $newEntry.reset();
+}
+
+function journalEntry(entry) {
+  // <li class="entries-list-item">
+  //   <div class="row">
+  //     <div class="column-half">
+  //       <img>
+  //     </div>
+
+  //     <div class="column-half">
+  //         <h2 class="entry-heading">title</h2>
+  //         <p class="entry-par">notes</p>
+  //     </div>
+  //   </div>
+  // </li>
+
+  var $li = document.createElement('li');
+  $li.className = 'entries-list-item';
+
+  var $row = document.createElement('div');
+  $row.className = 'row';
+  $li.appendChild($row);
+
+  var $imgCol = document.createElement('div');
+  $imgCol.className = 'column-half';
+  $row.appendChild($imgCol);
+
+  var $image = document.createElement('img');
+  $image.setAttribute('src', entry.url);
+  $image.className = 'max-width rounded img-preview';
+  $imgCol.appendChild($image);
+
+  var $textCol = docoment.createElement('div');
+  $textCol.className = 'column-half';
+  $row.appendChild($textCol);
+
+  var $entryTitle = document.createElement('h2');
+  $title.className = 'entry-heading';
+  $title.textContent = entry.title;
+  $textCol.appendChild($title);
+
+  var $entryNotes = document.createElement('p');
+  $entryNotes.className = 'entry-par';
+  $entryNotes.textContent = entry.notes;
+  $textCol.appendChild($entryNotes);
+
+  console.log($li);
+  return $li;
 }
